35aa58d25133be4ac77abcfa747c5386eb91f45f
Kevin Thrailkill <kevinjt92@gmail.com>
2017-04-25 11:54:17 -0700

    Very basic poi voting system complete

--- Travel With Friends/Travel With Friends/TripDetailPOIViewController.swift
+++ Travel With Friends/Travel With Friends/TripDetailPOIViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class TripDetailPOIViewController: UIViewController {
 
+    @IBOutlet weak var poiTableView: UITableView!
+    
+    
+    
+    var poiArray : [POI] = [POI(n: "new yrk"),POI(n: "big apple"),POI(n: "hello world"),POI(n: "temp 3"),POI(n: "4 temp"),POI(n: "temp"),POI(n: "temp"),POI(n: "temp"),POI(n: "temp")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        poiTableView.delegate = self
+        poiTableView.dataSource = self
+        
+        poiTableView.estimatedRowHeight = 90
+        
+        poiArray.sort {
+            $0.voteCount == $1.voteCount ? ($0.name < $1.name) : ($0.voteCount > $1.voteCount)
+        }
+        
+        
+        //load xib file
+        let nib = UINib(nibName: "POICell", bundle: nil)
+        poiTableView.register(nib, forCellReuseIdentifier: "POICell")
+        
         // Do any additional setup after loading the view.
     }
 
@@ -39,3 +60,53 @@ class TripDetailPOIViewController: UIViewController {
     */
 
 }
+
+extension TripDetailPOIViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "POICell") as! POICell
+        
+        cell.poi = poiArray[indexPath.row]
+        cell.delegate = self
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return poiArray.count
+    }
+    
+}
+
+extension TripDetailPOIViewController : POIVoteDelegate {
+    func sortPOI() {
+        poiArray.sort {
+            $0.voteCount == $1.voteCount ? ($0.name < $1.name) : ($0.voteCount > $1.voteCount)
+        }
+        poiTableView.reloadData()
+        
+    }
+}
+
+class POI {
+    var name : String = ""
+    var voteCount: Int = 0
+    var voteFor = false
+    
+    
+    init(n: String) {
+        name = n
+    }
+    
+    //need to change to dictionary of users that have voted
+    // var voted = [UserID : true] etc
+    
+}
+
+
+
+
+
+
+
