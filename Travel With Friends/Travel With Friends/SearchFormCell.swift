de2a8ccf8230aaf0dfdcd5028ba25298e650fd16
Janvier Wijaya <janvier_w@yahoo.com>
2017-05-06 13:47:22 -0700

    Add prototype for creating a new plan.

---
+++ Travel With Friends/Travel With Friends/SearchFormCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchFormCell.swift
+//  Travel With Friends
+//
+//  Created by Janvier Wijaya on 5/4/17.
+//  Copyright © 2017 kevinthrailkill. All rights reserved.
+//
+
+import UIKit
+
+class SearchFormCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
