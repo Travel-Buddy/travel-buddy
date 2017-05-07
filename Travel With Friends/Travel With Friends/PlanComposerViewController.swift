63e33e95b3376aa33d26a52339fcd1231eccc084
Kevin Thrailkill <kevinjt92@gmail.com>
2017-05-07 13:07:54 -0700

    Updated Merge

--- Travel With Friends/Travel With Friends/PlanComposerViewController.swift
+++ Travel With Friends/Travel With Friends/PlanComposerViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Janvier Wijaya on 4/30/17.
 //  Copyright © 2017 kevinthrailkill. All rights reserved.
 //
-
 import UIKit
 
 import Eureka
@@ -14,270 +13,237 @@ import GooglePlacesRow
 import Parse
 
 /* DEBUG CODE BEG */
-struct Plan {
-    var name = ""
-    var address: String?
-    var phoneNo: String?
-    var date: Date?
-}
-
-let debug = true
-
-func debugLog(_ varName: String?, _ varValue: Any?) {
-    if !debug {
-        return
-    }
-
-    if varName == nil {
-        print(" ")
-    } else if varValue == nil {
-        print("\(varName!):")
-    } else {
-        print("\(varName!): [\(varValue!)]")
-    }
-}
-
-/* Salt Lake City */
+/* Filter type: Points of Interest */
 /*
-let (debugNELat, debugNELng) = (40.8529699, -111.7394581)
-let (debugSWLat, debugSWLng) = (40.700246, -112.101512)
-*/
-/* Yellowstone National Park */
-
-let (debugNELat, debugNELng) = (45.0553667, -109.927147)
-let (debugSWLat, debugSWLng) = (44.1162236, -111.1009672)
-
-/* San Diego */
-/*
-let (debugNELat, debugNELng) = (33.114249, -116.90816)
-let (debugSWLat, debugSWLng) = (32.534856, -117.2821666)
-*/
-
-/* Points of Interest */
-/*
-let (debugNamePlaceFilterType, debugLocationPlaceFilterType) =
-        (GMSPlacesAutocompleteTypeFilter.establishment,
-         GMSPlacesAutocompleteTypeFilter.address)
-*/
-/* Natural Features */
+ let (debugNamePlaceFilterType, debugLocationPlaceFilterType) =
+ (GMSPlacesAutocompleteTypeFilter.establishment,
+ GMSPlacesAutocompleteTypeFilter.address)
+ */
+/* Filter type: Natural Features */
 
 let (debugNamePlaceFilterType, debugLocationPlaceFilterType) =
-        (GMSPlacesAutocompleteTypeFilter.geocode,
-         GMSPlacesAutocompleteTypeFilter.region)
+    (GMSPlacesAutocompleteTypeFilter.geocode,
+     GMSPlacesAutocompleteTypeFilter.region)
 
-
-let debugCoordinateBound = GMSCoordinateBounds(
-        coordinate: CLLocationCoordinate2D(latitude: debugNELat, longitude: debugNELng),
-        coordinate: CLLocationCoordinate2D(latitude: debugSWLat, longitude: debugSWLng))
 /* DEBUG CODE END */
 
 
 class PlanComposerViewController: FormViewController {
-    var plan: Plan?
-
+    var destination: PFObject!
+    var plan: PFObject?
+    
+    var coordinateBounds: GMSCoordinateBounds?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         form
-        +++ Section("Name")
+            +++ Section("Name")
             <<< GooglePlacesTableRow() {
                 $0.tag = "EstablishmentName"
                 $0.placeFilter?.type = debugNamePlaceFilterType
-                $0.placeBounds = debugCoordinateBound
-            }
-            .onChange(updateFormUsingGooglePlacesTableRow)
-
-        +++ Section("Location")
+                $0.placeBounds = self.coordinateBounds
+                
+                if let plan = self.plan,
+                    let name = plan["estabName"] as? String {
+                    $0.value = GooglePlace(string: name)
+                }
+                }
+                .onChange(updateFormUsingGooglePlacesTableRow)
+            
+            +++ Section("Location")
             <<< GooglePlacesTableRow() {
                 $0.tag = "EstablishmentLocation"
                 $0.placeFilter?.type = debugLocationPlaceFilterType
-                $0.placeBounds = debugCoordinateBound
-            }
-            .onChange { row in
-                let dictionary = self.form.values()
-
-                if let place = dictionary["EstablishmentLocation"] as? GooglePlace {
-                    switch place {
-                    case let GooglePlace.userInput(value: value):
-                        debugLog("userInput", value)
-                    case let GooglePlace.prediction(prediction: prediction):
-                        debugLog(nil, nil)
-                        debugLog("GooglePlace.prediction", nil)
-                        debugLog("  attributedFullText",
-                                prediction.attributedFullText.string)
-                        debugLog("  attributedPrimaryText",
-                                prediction.attributedPrimaryText.string)
-                        debugLog("  attributedSecondaryText",
-                                prediction.attributedSecondaryText?.string)
-                        debugLog("  placeID", prediction.placeID)
-                        debugLog("  types", prediction.types)
-                        debugLog(nil, nil)
-                    }
+                $0.placeBounds = self.coordinateBounds
+                
+                if let plan = self.plan,
+                    let location = plan["estabLocation"] as? String {
+                    $0.value = GooglePlace(string: location)
                 }
             }
-
-        +++ Section("Phone Number")
+            
+            +++ Section("Phone Number")
             <<< PhoneRow() {
                 $0.tag = "EstablishmentContact"
+                
+                if let plan = self.plan,
+                    let phoneNo = plan["estabContact"] as? String {
+                    $0.value = phoneNo
+                }
             }
-
-        +++ Section("Date")
+            
+            +++ Section("Date")
             <<< DateRow() {
                 $0.tag = "StartDate"
-
+                
+                let minDate = self.destination["startDate"] as? Date ?? Date()
+                let maxDate = self.destination["endDate"] as? Date
+                $0.minimumDate = minDate
+                $0.maximumDate = maxDate
+                
                 var calendar = Calendar(identifier: .gregorian)
                 calendar.timeZone = TimeZone(abbreviation: "UTC")!
-
                 $0.value = calendar.date(bySettingHour: 12, minute: 0,
-                        second: 0, of: Date()) ?? Date()
-            }
-
+                                         second: 0, of: minDate)
+        }
+        
         let nameRow = self.form.rowBy(tag: "EstablishmentName")
-                as! GooglePlacesTableRow
+            as! GooglePlacesTableRow
         nameRow.cell.textField.becomeFirstResponder()
-
-        /* DEBUG CODE BEG: Get geocode info of a place */
-        GooglePlacesAPIController.shared.getGeocode(address: "Salt Lake City") {
+        
+        /* Get coordinate bounds to be used in autocomplete function */
+        if let destinationName = destination["title"] as? String {
+            GooglePlacesAPIController.shared.getGeocode(
+            address: destinationName) {
                 (destinations: [GooglePlaceDestination]?, error: Error?) in
-                    if destinations != nil {
-                        for destination in destinations! {
-                            debugLog("Place ID", "\(destination.address)")
-                            debugLog("Lat, Lng",
-                                    "\(destination.geometryLocationLat)" +
-                                    ", \(destination.geometryLocationLng)")
-                            debugLog("NE Lat, Lng",
-                                    "\(destination.geometryViewportNELat)" +
-                                    ", \(destination.geometryViewportNELng)")
-                            debugLog("SW Lat, Lng",
-                                    "\(destination.geometryViewportSWLat)" +
-                                    ", \(destination.geometryViewportSWLng)")
-                            print("let (debugNELat, debugNELng) = (\(destination.geometryViewportNELat), \(destination.geometryViewportNELng))")
-                            print("let (debugSWLat, debugSWLng) = (\(destination.geometryViewportSWLat), \(destination.geometryViewportSWLng))")
-                        }
-                    } else if let error = error {
-                        print("ERROR: \(error.localizedDescription)")
+                if let error = error {
+                    print("ERROR: \(error.localizedDescription)")
+                } else if destinations != nil {
+                    for destination in destinations! {
+                        self.updateCoordinateBoundsUsingGPDestination(
+                            destination)
+                        /* Use the first destination */
+                        break
                     }
                 }
-        /* DEBUG CODE END */
+            }
+        }
     }
-
+    
+    private func updateCoordinateBoundsUsingGPDestination(
+        _ destination: GooglePlaceDestination) {
+        coordinateBounds = GMSCoordinateBounds(
+            coordinate:  CLLocationCoordinate2D(
+                latitude: destination.geometryViewportNELat,
+                longitude: destination.geometryViewportNELng),
+            coordinate: CLLocationCoordinate2D(
+                latitude: destination.geometryViewportSWLat,
+                longitude: destination.geometryViewportSWLng))
+        
+        let nameRow = form.rowBy(tag: "EstablishmentName")
+            as! GooglePlacesTableRow
+        nameRow.placeBounds = coordinateBounds
+        
+        let locationRow = form.rowBy(tag: "EstablishmentLocation")
+            as! GooglePlacesTableRow
+        locationRow.placeBounds = coordinateBounds
+    }
+    
     func updateFormUsingGooglePlacesTableRow(row: GooglePlacesTableRow) {
         let dictionary = form.values()
-
+        
         if let place = dictionary["EstablishmentName"] as? GooglePlace {
             switch place {
-            case let GooglePlace.userInput(value: value):
-                debugLog("userInput", value)
+            case GooglePlace.userInput(value: _):
+                break
             case let GooglePlace.prediction(prediction: prediction):
-                debugLog(nil, nil)
-                debugLog("GooglePlace.prediction", nil)
-                debugLog("  attributedFullText",
-                        prediction.attributedFullText.string)
-                debugLog("  attributedPrimaryText",
-                        prediction.attributedPrimaryText.string)
-                debugLog("  attributedSecondaryText",
-                        prediction.attributedSecondaryText?.string)
-                debugLog("  placeID", prediction.placeID)
-                debugLog("  types", prediction.types)
-                debugLog(nil, nil)
-
                 row.value = GooglePlace(
-                        string: prediction.attributedPrimaryText.string)
-
+                    string: prediction.attributedPrimaryText.string)
+                
                 if let placeID = prediction.placeID {
                     GooglePlacesAPIController.shared.getPlaceDetail(
-                            placeId: placeID) {
-                            (place: GooglePlacePlace?, error: Error?) in
-                                if let place = place {
-                                    debugLog(nil, nil)
-                                    debugLog("Place detail", nil)
-                                    debugLog("  ID", place.placeId)
-                                    debugLog("  Name", place.name)
-                                    debugLog("  Address", place.address)
-                                    debugLog("  Phone #", place.phoneNo)
-                                    debugLog("  Street #", place.streetNo)
-                                    debugLog("  Street", place.streetName)
-                                    debugLog("  City", place.city)
-                                    debugLog("  State", place.state)
-                                    debugLog("  ZIP code", place.zipCode)
-                                    debugLog("  Country", place.country)
-                                    debugLog(nil, nil)
-
-                                    let locationRow = self.form.rowBy(
-                                            tag: "EstablishmentLocation")
-                                            as! GooglePlacesTableRow
-                                    locationRow.value = GooglePlace(
-                                            string: place.address!)
-                                    locationRow.reload()
-
-                                    let phoneNoRow = self.form.rowBy(
-                                            tag: "EstablishmentContact")
-                                            as! PhoneRow
-                                    phoneNoRow.value = place.phoneNo
-                                    phoneNoRow.reload()
-                                } else if let error = error {
-                                    print("ERROR: \(error.localizedDescription)")
-                                }
-                            }
+                    placeId: placeID) {
+                        (place: GooglePlacePlace?, error: Error?) in
+                        if let error = error {
+                            print("ERROR: \(error.localizedDescription)")
+                        } else if let place = place {
+                            let locationRow = self.form.rowBy(
+                                tag: "EstablishmentLocation")
+                                as! GooglePlacesTableRow
+                            locationRow.value = GooglePlace(
+                                string: place.address!)
+                            locationRow.reload()
+                            
+                            let phoneNoRow = self.form.rowBy(
+                                tag: "EstablishmentContact")
+                                as! PhoneRow
+                            phoneNoRow.value = place.phoneNo
+                            phoneNoRow.reload()
+                        }
+                    }
                 }
             }
         }
     }
-
-    @IBAction func cancelChanges(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-
-    @IBAction func saveChanges(_ sender: Any) {
+    
+    private func composePlan(_ plan: PFObject) -> PFObject {
         let dictionary = form.values()
-        let dbPlan = PFObject(className: "Plan")
-
+        
         if let place = dictionary["EstablishmentName"] as? GooglePlace {
             switch place {
             case let GooglePlace.userInput(value: value):
-                dbPlan["estabName"] = value
+                plan["estabName"] = value
             case let GooglePlace.prediction(prediction: prediction):
-                dbPlan["estabName"] = prediction.attributedPrimaryText.string
+                plan["estabName"] = prediction.attributedPrimaryText.string
             }
         }
-
+        
         if let place = dictionary["EstablishmentLocation"] as? GooglePlace {
             switch place {
             case let GooglePlace.userInput(value: value):
-                dbPlan["estabLocation"] = value
+                plan["estabLocation"] = value
             case let GooglePlace.prediction(prediction: prediction):
-                dbPlan["estabLocation"] = prediction.attributedFullText.string
+                plan["estabLocation"] = prediction.attributedFullText.string
             }
         }
-
+        
         if let phoneNo = dictionary["EstablishmentContact"] as? String {
-            dbPlan["estabContact"] = phoneNo
+            plan["estabContact"] = phoneNo
         }
-
+        
         if let date = dictionary["StartDate"] as? Date {
-            dbPlan["startDate"] = date
+            plan["startDate"] = date
         }
-
-        dbPlan["planType"] = "natural_feature"
-        dbPlan["planStage"] = "proposal"
-        dbPlan["createdBy"] = PFUser.current()
-
-        debugLog(nil, nil)
-        debugLog("Plan to be saved", nil)
-        debugLog("  estabName", dbPlan["estabName"])
-        debugLog("  estabLocation", dbPlan["estabLocation"])
-        debugLog("  estabContact", dbPlan["estabContact"])
-        debugLog("  startDate", dbPlan["startDate"])
-        debugLog(nil, nil)
-
-        dbPlan.saveInBackground { (success, error) in
-            if success {
+        
+        plan["planType"] = "natural_feature"
+        plan["planStage"] = "proposal"
+        plan["createdBy"] = PFUser.current()
+        plan["destination"] = destination
+        
+        return plan
+    }
+    
+    private func addRelationPlan(_ plan: PFObject,
+                                 withDestination destination: PFObject) {
+        let relation = destination.relation(forKey: "plans")
+        relation.add(plan)
+        destination.saveInBackground {
+            (success: Bool, error: Error?) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            } else if success {
                 print("Plan is successfully saved!")
-            } else if error != nil {
-                print("ERROR: \(error!.localizedDescription)")
             }
         }
-
+    }
+    
+    private func savePlan() {
+        let query = PFQuery(className: "Plan")
+        query.getObjectInBackground(withId: plan?.objectId ?? "") {
+            (plan: PFObject?, error: Error?) in
+            let composedPlan = self.composePlan(
+                (error == nil && plan != nil ? plan! :
+                    PFObject(className: "Plan")))
+            
+            composedPlan.saveInBackground {
+                (success: Bool, error: Error?) in
+                if let error = error {
+                    print("ERROR: \(error.localizedDescription)")
+                } else if success {
+                    self.addRelationPlan(composedPlan,
+                                         withDestination: self.destination)
+                }
+            }
+        }
+    }
+    
+    @IBAction func cancelChanges(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveChanges(_ sender: Any) {
+        savePlan()
         dismiss(animated: true, completion: nil)
     }
 }
