20597b5bf63ac5f012b8206b8dd83111bd7f86ae
Curtis Wilcox <curtis@devfountain.com>
2017-05-10 21:49:11 -0600

    Completed Milestone 3

--- Travel With Friends/Travel With Friends/DestinationCell.swift
+++ Travel With Friends/Travel With Friends/DestinationCell.swift
@@ -11,7 +11,23 @@ import UIKit
 
 class DestinationCell: PFTableViewCell {
 
-    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var dateRangeLabel: UILabel!
+
+    var destination: PFObject! {
+        didSet {
+            titleLabel.text = destination["title"] as? String
+            subtitleLabel.text = destination["subtitle"] as? String
+
+            if let startDate = destination["startDate"] as? Date, let endDate = destination["endDate"] as? Date {
+                if endDate.compare(startDate) == .orderedSame {
+                    dateRangeLabel.text = startDate.asString()
+                } else {
+                    dateRangeLabel.text = startDate.asString() + " - " + endDate.asString()
+                }
+            }
+        }
+    }
 
 }
